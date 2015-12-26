bc1798df96a5c83ff7ed0f93fe71fd2132d5d6be
Lorenz <lorenzbischof@gmail.com>
2015-12-26 16:42:09 +0100

    add some custom markdown

--- render.rb
+++ render.rb
@@ -1,42 +1,54 @@
 class CustomMarkdown < Redcarpet::Render::HTML
-  def initialize(options={})
-    super options.merge(
-      :with_toc_data                => true,
-    )
-  end
-
- def preprocess(document)
-
-    # We need this renderer to force render of content within our multi-line markdown elements
-    # Note this is the same config used in config.rb for the generic middleman renderer
-    r = Redcarpet::Markdown.new(self, options = {
-      :markdown => true,
-      :fenced_code_blocks           => true,
-      :no_intra_emphasis            => true,
-      :tables                       => true,
-      :autolink                     => true,
-      :disable_indented_code_blocks => true,
-      :quote                        => true,
-      :lax_spacing                  => true
-    })
-
-    # Render all of our custom markdown elements
-    rendered_doc = custom_content_card(document, r)
-  end
-
-  def custom_content_card(document, renderer)
-    document.gsub(/^\[\[(.*?)\n(.*?)\n([^\n]*?)\]\]/m) do
-      resp = "<div class=\"panel panel-default\">
-        <div class=\"panel-heading\">
-          <div class=\"panel-title\">
-          #{$1}
-          </div>
-        </div>
-        <div class=\"panel-body\">#{renderer.render($2)}</div>
-        <div class=\"panel-footer\">#{$3}</div>
-      </div>"
-      resp
+    def initialize(options={})
+        super options.merge(
+            :with_toc_data                => true,
+        )
     end
-  end
 
+    def preprocess(document)
+
+        # We need this renderer to force render of content within our multi-line markdown elements
+        # Note this is the same config used in config.rb for the generic middleman renderer
+        r = Redcarpet::Markdown.new(self, options = {
+            :markdown => true,
+            :autolink                     => true,
+            :lax_spacing                  => true
+        })
+        # Render all of our custom markdown elements
+        rendered_doc = mals_item(document, r)
+        rendered_doc = mals_form(rendered_doc, r)
+        rendered_doc = mals_radio(rendered_doc, r)
+    end
+
+    def mals_form(document, r)
+        document.gsub(/!!form(.*?)!!cart/m) do
+            resp = <<-HTML
+<form action="http://ww8.aitsafe.com/cf/addmulti.cfm" method="post">
+    <input type="hidden" name="userid" value="A4132041">
+    <input type="hidden" name="return" value="www.maplesplendor.ca/dollsandsupplies.htm">
+    #{r.render($1)}
+    <button>Cart</button>
+</form>
+            HTML
+            resp
+        end
+    end
+
+    def mals_item(document, r)
+        document.gsub(/^!!item (.*)/) do
+            resp = <<-HTML
+<div class="item">#{$1}</div>
+            HTML
+            resp
+        end
+    end
+
+    def mals_radio(document, r)
+        document.gsub(/^!!radio (.*) ([0-9]+)$/) do
+            resp = <<-HTML
+<input name="productpr1" type="radio">#{$1} #{$2}</input>
+            HTML
+            resp
+        end
+    end
 end
--- source/test.html.md
+++ source/test.html.md
@@ -1,7 +1,16 @@
 # Test
 
-[[Title
-body
-ateiat
 testing
-footer]]
+
+!!form
+!!item iea
+!!item eaiea tralala.jpg
+!!radio test 10
+!!radio something 20
+!!cart
+
+!!form
+!!item aieaiea
+!!radio 20 gramms 2
+!!radio 30 gramms 4
+!!cart
