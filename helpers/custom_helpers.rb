c11c6418379a47be4377a40187ea05a172fe3387
Lorenz <lorenzbischof@gmail.com>
2015-12-27 13:39:03 +0100

    small steps

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,5 +1,8 @@
 module CustomHelpers
     def mals_multiple product
-        partial "partials/mals_multiple", locals: { product: $product }
+        partial "partials/mals_multiple", locals: { product: product }
+    end
+    def current_page_data product
+        data.public_send(current_page.path.split('.')[0]).public_send(product)
     end
 end
