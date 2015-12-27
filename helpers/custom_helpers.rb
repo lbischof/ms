f1f4cd4398e104adb9dc572c29c413ec6c659525
Lorenz <lorenzbischof@gmail.com>
2015-12-27 20:53:36 +0100

    add basic error handling and remove default page

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,8 +1,25 @@
 module CustomHelpers
     def mals_multiple product
-        partial "partials/mals_multiple", locals: { product: product }
+        begin
+            partial "partials/mals_multiple", locals: { product: product }
+        rescue Exception => msg
+            puts "Error in CustomHelpers."
+            puts "Page: #{current_page.path} Product: #{product}"
+            puts msg
+            raise
+        end
     end
     def current_page_data product
-        data.public_send(current_page.path.split('.')[0]).public_send(product)
+        begin
+            data.public_send(current_page.path.split('.')[0]).public_send(product)
+        rescue Exception => msg
+            puts "Error in CustomHelpers."
+            puts "Page: #{current_page.path} Product: #{product}"
+            puts msg
+            raise
+        end
+    end
+    def product_image_tag product, colour
+        image_tag "/images/#{current_page.path.split('.')[0]}/#{product}/#{colour.downcase.gsub(/[^0-9a-z]/, '')}.jpg"
     end
 end
