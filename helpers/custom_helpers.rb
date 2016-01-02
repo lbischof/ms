1a559dc6b71251329d1f6555c5a911800dc02439
Lorenz <lorenzbischof@gmail.com>
2016-01-02 23:58:59 +0100

    update mals-multiple partial

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,5 +1,5 @@
 module CustomHelpers
-    def product_image_tag product, colour
+    def product_image_tag colour
         image_tag "#{current_page.path.split('.')[0]}/#{colour.downcase.gsub(/[^0-9a-z]/, '')}.jpg"
     end
 
