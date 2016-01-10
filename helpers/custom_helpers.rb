3e432e509dfe37a83adef6fd10221e285717a72a
Lorenz <lorenzbischof@gmail.com>
2016-01-10 18:35:09 +0100

    add images and product descriptions

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,10 +1,10 @@
 module CustomHelpers
-    def colour_image_tag colour
-        image_tag "#{current_page.path.split('.')[0]}/#{colour.downcase.gsub(/[^0-9a-z](\(.*\))?/, '')}.jpg"
+    def colour_image_tag(product, colour)
+        image_tag "#{current_page.path.split('.')[0]}/#{product.downcase}/#{colour.downcase.gsub(/[^0-9a-z](\(.*\))?/, '')}.jpg"
     end
 
     def banner_image_tag product
-        "<div class=\"banner\" style=\"background-image: url('images/#{current_page.path.split('.')[0]}/banner-#{product.downcase}.jpg')\"></div>"
+        "<div class=\"banner\" style=\"background-image: url('images/#{current_page.path.split('.')[0]}/#{product.downcase}/banner-#{product.downcase}.jpg')\"></div>"
     end
 
     def includepartial pdata
