3fdb44021ed836aa6543e0cc29dc7c55644710de
Lorenz <lorenzbischof@gmail.com>
2016-01-12 15:55:22 +0100

    cleanup helpers and add nav_link helper

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,10 +1,22 @@
 module CustomHelpers
+    def get_current_page
+        if current_page.url == '/'
+            "index"
+        else
+            current_page.url.gsub('/','')
+        end
+    end
+
+    def sanitize string
+        string.downcase.gsub(/[^0-9a-z](\(.*\))?/, '')
+    end
+
     def colour_image_tag(product, colour)
-        image_tag "#{current_page.path.split('.')[0]}/#{product.downcase}/#{colour.downcase.gsub(/[^0-9a-z](\(.*\))?/, '')}.jpg"
+        image_tag "#{get_current_page}/#{sanitize product}/#{sanitize colour}.jpg"
     end
 
     def banner_image_tag product
-        "<div class=\"banner\" style=\"background-image: url('images/#{current_page.path.split('.')[0]}/#{product.downcase}/banner-#{product.downcase}.jpg')\"></div>"
+        "<div class=\"banner\" style=\"background-image: url('images/#{get_current_page}/#{sanitize product}/banner-#{sanitize product}.jpg')\"></div>"
     end
 
     def includepartial pdata
@@ -13,5 +25,11 @@ module CustomHelpers
 
     def markdown text
         Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(text)
+    end
+
+    def nav_link(link_text, url, options = {})
+        options[:class] ||= ""
+        options[:class] << " active" if url == current_page.url
+        link_to(link_text, url, options)
     end
 end
